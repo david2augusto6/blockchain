97c32ad0c98787f0b05d15395f36f2bdaee0d20a
David Silva <david11augusto11@gmail.com>
2025-11-15 12:43:59 -0400

    consensus

--- blockchain.py
+++ blockchain.py
@@ -3,6 +3,7 @@ import json
 from time import time
 from textwrap import dedent
 from uuid import uuid4
+from urllib.parse import urlparse
 
 from flask import Flask
 
@@ -11,10 +12,21 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.nodes = set()
 
         # Cria o bloco "gênese"
         self.new_block(previous_hash=1, proof=100)
     
+    def register_node(self, address):
+        """
+            Adiciona um nó à lista de nós
+            :param address: <str> Endereço do nó. Ex.: 'htp://192.168.0.5:5000'
+            :return: None
+        """
+
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
     def new_transaction(self, sender, recipient, amount):
         """
             Cria uma nova transação que irá para o próximo bloco minerado
@@ -99,3 +111,64 @@ class Blockchain(object):
         return guess_hash[:4] == "0000"
 
 
+    def valid_chain(self, chain):
+        """
+            Determina se uma blockchain é válida
+            :param chain: <list> Uma blockchain
+            :return: <bool> True se válido, False se não.
+        """
+
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+
+            #Checa se o hash está correto
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+            
+            #Checa se a Proof of Work está correta
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+            
+            last_block = block
+            current_index += 1
+        
+        return True
+    
+    def resolve_conflicts(self):
+        """
+            Algoritmo de Consenso. Ele resolve conflitos substituindo
+            nossa cadeia pela mais longa da rede.
+            :return: <bool> True se a cadeia foi substituída, False se não
+        """
+
+        neighbours = self.nodes
+        new_chain = None
+        
+        # Procuramos apenas por cadeias mais longas
+        max_lenght = len(self.chain)
+
+        # Seleciona e verifica cadeias de todos os nós da rede
+        for node in neighbours:
+            response = request.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # checa se o comprimento é maior e se a cadeia é válida
+                if length > max_length and self.valid_chain(chain):
+                    max_lenght = length
+                    new_chain = chain
+            
+            # Substitui nossa cadeia se descobrirmos uma nova cadeia válida e maior
+            if new_chain:
+                self.chain = new_chain
+                return True
+            
+            return False
